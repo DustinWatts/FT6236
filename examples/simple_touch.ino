1374df187e641cf471891314996348f28a387353
Dustin Watts <dustin_watts@yahoo.com>
2020-10-18 12:57:49 +0200

    Create simple_touch.ino

---
+++ examples/simple_touch.ino
@@ -0,0 +1,43 @@
+/* 
+Title: FT6236 Simple touch example.
+By: Dustin Watts
+Date: October 18th 2020
+
+Description: 
+
+This sketch checks if the screen is touched and if so, prints the coordinates
+to the serial output.
+*/
+
+#include <FT6236.h>
+
+FT6236 ts = FT6236();
+
+void setup(void)
+{
+    Serial.begin(9600);
+
+    if (!ts.begin(40))
+    {
+        Serial.println("Unable to start the capacitive touchscreen.");
+    }
+}
+
+void loop(void)
+{
+
+    if (ts.touched())
+    {
+        // Retrieve a point
+        TS_Point p = ts.getPoint();
+
+        // Print coordinates to the serial output
+        Serial.print("X Coordinate: ");
+        Serial.println(p.x);
+        Serial.print("Y Coordinate: ");
+        Serial.println(p.y);
+    }
+
+    //Debouncing. To avoid returning the same touch multiple times you can play with this delay.
+    delay(50);
+}
