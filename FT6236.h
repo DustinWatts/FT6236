3fd719bea73e5f96fed3bf65fc705ae5dfce3c2a
Dustin Watts <dustin_watts@yahoo.com>
2020-10-18 10:09:30 +0200

    Version 1.0.1

--- FT6236.h
+++ FT6236.h
@@ -8,28 +8,28 @@
 #include "Arduino.h"
 #include <Wire.h>
 
-#define FT6236_ADDR 0x38            //!< I2C address
-#define FT6236_G_FT5201ID 0xA8      //!< FocalTech's panel ID
-#define FT6236_REG_NUMTOUCHES 0x02  //!< Number of touch points
+#define FT6236_ADDR 0x38            // I2C address
+#define FT6236_G_FT5201ID 0xA8      // FocalTech's panel ID
+#define FT6236_REG_NUMTOUCHES 0x02  // Number of touch points
 
-#define FT6236_NUM_X 0x33           //!< Touch X position
-#define FT6236_NUM_Y 0x34           //!< Touch Y position
+#define FT6236_NUM_X 0x33           // Touch X position
+#define FT6236_NUM_Y 0x34           // Touch Y position
 
-#define FT6236_REG_MODE 0x00        //!< Device mode, either WORKING or FACTORY
-#define FT6236_REG_CALIBRATE 0x02   //!< Calibrate mode
-#define FT6236_REG_WORKMODE 0x00    //!< Work mode
-#define FT6236_REG_FACTORYMODE 0x40 //!< Factory mode
-#define FT6236_REG_THRESHHOLD 0x80  //!< Threshold for touch detection
-#define FT6236_REG_POINTRATE 0x88   //!< Point rate
-#define FT6236_REG_FIRMVERS 0xA6    //!< Firmware version
-#define FT6236_REG_CHIPID 0xA3      //!< Chip selecting
-#define FT6236_REG_VENDID 0xA8      //!< FocalTech's panel ID
+#define FT6236_REG_MODE 0x00        // Device mode, either WORKING or FACTORY
+#define FT6236_REG_CALIBRATE 0x02   // Calibrate mode
+#define FT6236_REG_WORKMODE 0x00    // Work mode
+#define FT6236_REG_FACTORYMODE 0x40 // Factory mode
+#define FT6236_REG_THRESHHOLD 0x80  // Threshold for touch detection
+#define FT6236_REG_POINTRATE 0x88   // Point rate
+#define FT6236_REG_FIRMVERS 0xA6    // Firmware version
+#define FT6236_REG_CHIPID 0xA3      // Chip selecting
+#define FT6236_REG_VENDID 0xA8      // FocalTech's panel ID
 
-#define FT6236_VENDID 0x11  //!< FocalTech's panel ID
-#define FT6236_CHIPID 0x36  //!< Chip selecting
-#define FT6236U_CHIPID 0x64 //!< Chip selecting
+#define FT6236_VENDID 0x11  // FocalTech's panel ID
+#define FT6236_CHIPID 0x36  // FT6236 ID
+#define FT6236U_CHIPID 0x64 // FT6236U ID
 
-#define FT6236_DEFAULT_THRESHOLD 128 //!< Default threshold for touch detection
+#define FT6236_DEFAULT_THRESHOLD 128 // Default threshold for touch detection
 
 class TS_Point
 {
