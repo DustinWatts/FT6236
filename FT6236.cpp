b42c636aaba5b96ebda8c9926f4b3f647f9fcbf6
Dustin Watts <dustin_watts@yahoo.com>
2020-10-18 09:40:22 +0200

    Version 1.0.1

---
+++ FT6236.cpp
@@ -0,0 +1,149 @@
+/*
+This is a library for the FT6236 touchscreen controller by FocalTech.
+
+The FT6236 and FT6236u work the same way.
+
+Inspired by Limor Fried/Ladyada
+*/
+
+#include "Arduino.h"
+#include "FT6236.h"
+#include <Wire.h>
+
+/* New class. */
+FT6236::FT6236() { touches = 0; }
+
+/* Start I2C and check if the FT6236 is found. */
+boolean FT6236::begin(uint8_t thresh)
+{
+    Wire.begin();
+
+    // Adjust threshold
+    writeRegister8(FT6236_REG_THRESHHOLD, thresh);
+
+    // Check if our chip has the correct Vendor ID
+    if (readRegister8(FT6236_REG_VENDID) != FT6236_VENDID)
+    {
+        return false;
+    }
+    //Check if our chip has the correct Chip ID.
+    uint8_t id = readRegister8(FT6236_REG_CHIPID);
+    if ((id != FT6236_CHIPID) && (id != FT6236U_CHIPID)) {
+    return false;
+    }
+
+    return true;
+}
+
+/* Returns the number of touched */
+uint8_t FT6236::touched(void)
+{
+    uint8_t n = readRegister8(FT6236_REG_NUMTOUCHES);
+    if (n > 2)
+    {
+        n = 0;
+    }
+    return n;
+}
+
+/* Get a touch point */
+TS_Point FT6236::getPoint(uint8_t n)
+{
+    readData();
+    if ((touches == 0) || (n > 1))
+    {
+        return TS_Point(0, 0, 0);
+    }
+    else
+    {
+        return TS_Point(touchX[n], touchY[n], 1);
+    }
+}
+
+void FT6236::readData(void)
+{
+
+    uint8_t i2cdat[16];
+    Wire.beginTransmission(FT6236_ADDR);
+    Wire.write((byte)0);
+    Wire.endTransmission();
+
+    Wire.requestFrom((byte)FT6236_ADDR, (byte)16);
+    for (uint8_t i = 0; i < 16; i++)
+        i2cdat[i] = Wire.read();
+
+    touches = i2cdat[0x02];
+    if ((touches > 2) || (touches == 0))
+    {
+        touches = 0;
+    }
+
+    for (uint8_t i = 0; i < 2; i++)
+    {
+        touchX[i] = i2cdat[0x03 + i * 6] & 0x0F;
+        touchX[i] <<= 8;
+        touchX[i] |= i2cdat[0x04 + i * 6];
+        touchY[i] = i2cdat[0x05 + i * 6] & 0x0F;
+        touchY[i] <<= 8;
+        touchY[i] |= i2cdat[0x06 + i * 6];
+        touchID[i] = i2cdat[0x05 + i * 6] >> 4;
+    }
+}
+
+/* Reading a byte from a register */
+uint8_t FT6236::readRegister8(uint8_t reg)
+{
+    uint8_t x;
+
+    Wire.beginTransmission(FT6236_ADDR);
+    Wire.write((byte)reg);
+    Wire.endTransmission();
+
+    Wire.requestFrom((byte)FT6236_ADDR, (byte)1);
+    x = Wire.read();
+
+    return x;
+}
+
+/* Writing a byte to a register */
+void FT6236::writeRegister8(uint8_t reg, uint8_t val)
+{
+
+    Wire.beginTransmission(FT6236_ADDR);
+    Wire.write((byte)reg);
+    Wire.write((byte)val);
+    Wire.endTransmission();
+}
+
+/* Debug */
+void FT6236::debug(void)
+{
+    Serial.print("Vend ID: 0x");
+    Serial.println(readRegister8(FT6236_REG_VENDID), HEX);
+    Serial.print("Chip ID: 0x");
+    Serial.println(readRegister8(FT6236_REG_CHIPID), HEX);
+    Serial.print("Firm V: ");
+    Serial.println(readRegister8(FT6236_REG_FIRMVERS));
+    Serial.print("Point Rate Hz: ");
+    Serial.println(readRegister8(FT6236_REG_POINTRATE));
+    Serial.print("Thresh: ");
+    Serial.println(readRegister8(FT6236_REG_THRESHHOLD));
+}
+
+TS_Point::TS_Point(void) { x = y = z = 0; }
+
+TS_Point::TS_Point(int16_t _x, int16_t _y, int16_t _z) {
+  x = _x;
+  y = _y;
+  z = _z;
+}
+
+/* == comparator between two points */
+bool TS_Point::operator==(TS_Point p1) {
+  return ((p1.x == x) && (p1.y == y) && (p1.z == z));
+}
+
+/* != comparator netween two points */
+bool TS_Point::operator!=(TS_Point p1) {
+  return ((p1.x != x) || (p1.y != y) || (p1.z != z));
+}
---
+++ FT6236.h
@@ -0,0 +1,65 @@
+/*!
+ * FT6236.h
+ */
+
+#ifndef FT6236_H
+#define FT6236_H
+
+#include "Arduino.h"
+#include <Wire.h>
+
+#define FT6236_ADDR 0x38            //!< I2C address
+#define FT6236_G_FT5201ID 0xA8      //!< FocalTech's panel ID
+#define FT6236_REG_NUMTOUCHES 0x02  //!< Number of touch points
+
+#define FT6236_NUM_X 0x33           //!< Touch X position
+#define FT6236_NUM_Y 0x34           //!< Touch Y position
+
+#define FT6236_REG_MODE 0x00        //!< Device mode, either WORKING or FACTORY
+#define FT6236_REG_CALIBRATE 0x02   //!< Calibrate mode
+#define FT6236_REG_WORKMODE 0x00    //!< Work mode
+#define FT6236_REG_FACTORYMODE 0x40 //!< Factory mode
+#define FT6236_REG_THRESHHOLD 0x80  //!< Threshold for touch detection
+#define FT6236_REG_POINTRATE 0x88   //!< Point rate
+#define FT6236_REG_FIRMVERS 0xA6    //!< Firmware version
+#define FT6236_REG_CHIPID 0xA3      //!< Chip selecting
+#define FT6236_REG_VENDID 0xA8      //!< FocalTech's panel ID
+
+#define FT6236_VENDID 0x11  //!< FocalTech's panel ID
+#define FT6236_CHIPID 0x36  //!< Chip selecting
+#define FT6236U_CHIPID 0x64 //!< Chip selecting
+
+#define FT6236_DEFAULT_THRESHOLD 128 //!< Default threshold for touch detection
+
+class TS_Point
+{
+public:
+    TS_Point(void);
+    TS_Point(int16_t x, int16_t y, int16_t z);
+
+    bool operator==(TS_Point);
+    bool operator!=(TS_Point);
+
+    int16_t x;
+    int16_t y;
+    int16_t z;
+};
+
+class FT6236 {
+public:
+  FT6236(void);
+  void debug(void);
+  boolean begin(uint8_t thresh = FT6236_DEFAULT_THRESHOLD);
+  uint8_t touched(void);
+  TS_Point getPoint(uint8_t n = 0);
+
+private:
+  void writeRegister8(uint8_t reg, uint8_t val);
+  uint8_t readRegister8(uint8_t reg);
+
+  void readData(void);
+  uint8_t touches;
+  uint16_t touchX[2], touchY[2], touchID[2];
+};
+
+#endif
